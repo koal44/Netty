e50d0b545901677169420e14ba258fe6db2cce14
koal <random.var.ny@gmail.com>
2023-09-25 16:30:24 -0700

    Re-add unit tests from previous solution

---
+++ Tests/FilterDataTests.cs
@@ -0,0 +1,231 @@
+using FluentAssertions;
+using Netryoshka;
+using System.Net;
+using static Netryoshka.BasicPacket;
+
+namespace Proton.Tests
+{
+    public class FilterDataTests
+    {
+
+#pragma warning disable IDE0230 // Use UTF-8 string literal
+#pragma warning disable CA1825 // Avoid zero-length array allocations
+
+
+        [Fact]
+        public void ShouldKeepPacket_RemotePortMatchesOutgoingSrc_ReturnsTrue()
+        {
+            // Assert
+            var filterData = new FilterData(remotePorts: new HashSet<int> { 1002 });
+            var packet = new BasicPacket(
+                IPAddress.Parse("192.168.1.1"), 1001,
+                IPAddress.Parse("192.168.1.2"), 1002,
+                new byte[] { }, DateTime.Now,
+                BPProtocol.TCP)
+            {
+                Direction = BPDirection.Outgoing
+            };
+
+            // Act 
+            var result = filterData.ShouldKeepPacket(packet);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShouldKeepPacket_RemotePortNoMatchOutgoingSrc_ReturnsFalse()
+        {
+            // Assert
+            var filterData = new FilterData(remotePorts: new HashSet<int> { 1003 });
+            var packet = new BasicPacket(
+                IPAddress.Parse("192.168.1.1"), 1001,
+                IPAddress.Parse("192.168.1.2"), 1002,
+                new byte[] { }, DateTime.Now,
+                BPProtocol.TCP)
+            {
+                Direction = BPDirection.Outgoing
+            };
+
+            // Act 
+            var result = filterData.ShouldKeepPacket(packet);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+
+
+        [Fact]
+        public void ShouldKeepPacket_RemotePortMatchesIncomingDst_ReturnsFalse()
+        {
+            // Assert
+            var filterData = new FilterData(remotePorts: new HashSet<int> { 1002 });
+            var packet = new BasicPacket(
+                IPAddress.Parse("192.168.1.1"), 1001,
+                IPAddress.Parse("192.168.1.2"), 1002,
+                new byte[] { }, DateTime.Now,
+                BPProtocol.TCP)
+            {
+                Direction = BPDirection.Incoming
+            };
+
+            // Act 
+            var result = filterData.ShouldKeepPacket(packet);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldKeepPacket_RemotePortMatchesUnknownSrc_ReturnsTrue()
+        {
+            // Assert
+            var filterData = new FilterData(remotePorts: new HashSet<int> { 1001 });
+            var packet = new BasicPacket(
+                IPAddress.Parse("192.168.1.1"), 1001,
+                IPAddress.Parse("192.168.1.2"), 1002,
+                new byte[] { }, DateTime.Now,
+                BPProtocol.TCP)
+            {
+                Direction = BPDirection.Unknown
+            };
+
+            // Act 
+            var result = filterData.ShouldKeepPacket(packet);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+
+        [Fact]
+        public void ShouldKeepPacket_EmptyRemotePorts_ReturnsFalse() // can i test all three directions within 1 test?
+        {
+            // Assert
+            var filterData = new FilterData(remotePorts: new HashSet<int> { });
+            var packet = new BasicPacket(
+                IPAddress.Parse("192.168.1.1"), 1001,
+                IPAddress.Parse("192.168.1.2"), 1002,
+                new byte[] { }, DateTime.Now,
+                BPProtocol.TCP)
+            {
+                Direction = BPDirection.Outgoing
+            };
+
+            // Act 
+            var result = filterData.ShouldKeepPacket(packet);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldKeepPacket_MultipleRemotePortsOnlyOneMatchesOutgoingSrc_ReturnsTrue()
+        {
+            // Assert
+            var filterData = new FilterData(remotePorts: new HashSet<int> { 999, 1000, 1002 });
+            var packet = new BasicPacket(
+                IPAddress.Parse("192.168.1.1"), 1001,
+                IPAddress.Parse("192.168.1.2"), 1002,
+                new byte[] { }, DateTime.Now,
+                BPProtocol.TCP)
+            {
+                Direction = BPDirection.Outgoing
+            };
+
+            // Act 
+            var result = filterData.ShouldKeepPacket(packet);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ShouldKeepPacket_RemotePortNoMatchUnknownSrc_ReturnsFalse()
+        {
+            // Assert
+            var filterData = new FilterData(remotePorts: new HashSet<int> { 999, 1000 });
+            var packet = new BasicPacket(
+                IPAddress.Parse("192.168.1.1"), 1001,
+                IPAddress.Parse("192.168.1.2"), 1002,
+                new byte[] { }, DateTime.Now,
+                BPProtocol.TCP)
+            {
+                Direction = BPDirection.Unknown
+            };
+
+            // Act 
+            var result = filterData.ShouldKeepPacket(packet);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldKeepPacket_RemotePortMatchesBothSrcAndDst_ReturnsTrue() // test multiple directons?
+        {
+            // Assert
+            var filterData = new FilterData(remotePorts: new HashSet<int> { 3333 });
+            var packet = new BasicPacket(
+                IPAddress.Parse("192.168.1.1"), 3333,
+                IPAddress.Parse("192.168.1.2"), 3333,
+                new byte[] { }, DateTime.Now,
+                BPProtocol.TCP)
+            {
+                Direction = BPDirection.Outgoing
+            };
+
+            // Act 
+            var result = filterData.ShouldKeepPacket(packet);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+#pragma warning restore IDE0230 // Use UTF-8 string literal
+#pragma warning restore CA1825 // Avoid zero-length array allocations
+
+
+
+        //[Fact]
+        //public void ShouldKeepPacket_IncomingPacketMatchesRemotePort_ReturnsFalse()
+        //{
+        //    // Arrange
+        //    var filterData = new FilterData(remotePorts: new HashSet<int> { 80 });
+        //    var packet = new BasicPacket(
+        //        IPAddress.Parse("192.168.1.1"), 1000,
+        //        IPAddress.Parse("192.168.1.2"), 80,
+        //        new byte[] { }, DateTime.Now,
+        //        BPProtocol.TCP);
+        //    packet.BPDirection = BPDirection.Pivot;
+
+        //    // Act 
+        //    var result = filterData.ShouldKeepPacket(packet);
+
+        //    // Assert
+        //    result.Should().BeFalse();
+        //}
+
+
+        //[Fact]
+        //public void ShouldKeepPacket_UnknownDirectionPacketMatchesRemotePort_ReturnsTrue()
+        //{
+        //    // Arrange
+        //    var filterData = new FilterData(remotePorts: new HashSet<int> { 80 });
+        //    var packet = new BasicPacket(
+        //        IPAddress.Parse("192.168.1.1"), 1000,
+        //        IPAddress.Parse("192.168.1.2"), 80,
+        //        new byte[] { }, DateTime.Now,
+        //        BPProtocol.TCP);
+        //    packet.BPDirection = BPDirection.Other;
+
+        //    // Act 
+        //    var result = filterData.ShouldKeepPacket(packet);
+
+        //    // Assert
+        //    result.Should().BeTrue();
+        //}
+    }
+
+
+}
---
+++ Tests/FlowManagerTest.cs
@@ -0,0 +1,165 @@
+﻿using FluentAssertions;
+using Netryoshka;
+using Netryoshka.Services;
+using System.Net;
+using System.Net.NetworkInformation;
+using static Netryoshka.BasicPacket;
+
+namespace Proton.Tests
+{
+    public class FlowManagerTests
+    {
+        private static BasicPacket CreatePacket(IPAddress sourceAddress, ushort sourcePort, IPAddress destAddress, ushort destPort, byte[]? payload = null, DateTime? timestamp = null, BPProtocol? protocolType = null)
+            => new(
+                sourceAddress, sourcePort,
+                destAddress, destPort,
+                payload ?? Array.Empty<byte>(),
+                timestamp ?? DateTime.Now,
+                protocolType ?? BPProtocol.TCP);
+
+
+        [Fact]
+        public void ShouldGroupPacketsByFlows()
+        {
+            // Arrange
+            var service = new FlowManager();
+
+            var packet1 = CreatePacket(
+                IPAddress.Parse("1.1.1.1"), 22,
+                IPAddress.Parse("8.8.8.8"), 99);
+
+            var packet2 = CreatePacket(
+                IPAddress.Parse("2.2.2.2"), 33,
+                IPAddress.Parse("8.8.8.8"), 99);
+
+            var packet3 = CreatePacket(
+                IPAddress.Parse("1.1.1.1"), 22,
+                IPAddress.Parse("8.8.8.8"), 99);
+
+            // Act
+            service.AddPacket(packet1);
+            service.AddPacket(packet2);
+            service.AddPacket(packet3);
+
+            // Assert
+            var allFlows = service.GetAllFlows();
+            allFlows.Should().HaveCount(2); // Two unique flows
+
+            allFlows[packet1.FlowKey].Should().HaveCount(2); // Two packets in the first flow
+            allFlows[packet2.FlowKey].Should().HaveCount(1); // One packet in the second flow
+        }
+
+
+        [Fact]
+        public void ShouldGroupIntoSameFlowDespiteDirection()
+        {
+            // Arrange
+            var service = new FlowManager();
+
+            var packet1 = CreatePacket(
+                IPAddress.Parse("1.1.1.1"), 22,
+                IPAddress.Parse("8.8.8.8"), 99);
+
+            var packet2 = CreatePacket(
+                IPAddress.Parse("8.8.8.8"), 99,
+                IPAddress.Parse("1.1.1.1"), 22);
+
+            var packet3 = CreatePacket(
+                IPAddress.Parse("1.1.1.1"), 22,
+                IPAddress.Parse("8.8.8.8"), 99);
+
+            // Act
+            service.AddPacket(packet1);
+            service.AddPacket(packet2);
+            service.AddPacket(packet3);
+
+            // Assert
+            var allFlows = service.GetAllFlows();
+            allFlows.Should().HaveCount(1); // One unique flow
+
+            // Since all three packets are part of the same flow, 
+            // the length of the list for that flow should be 3.
+            allFlows[packet1.FlowKey].Should().HaveCount(3);
+        }
+
+
+        [Fact]
+        public void ShouldHandleDuplicatePacketsInSameFlow()
+        {
+            // Arrange
+            var service = new FlowManager();
+
+            var packet1 = CreatePacket(
+                IPAddress.Parse("1.1.1.1"), 22,
+                IPAddress.Parse("8.8.8.8"), 99);
+
+            // Act
+            service.AddPacket(packet1);
+            service.AddPacket(packet1);  // Duplicate
+
+            // Assert
+            var allFlows = service.GetAllFlows();
+            allFlows[packet1.FlowKey].Should().HaveCount(2); // Two packets, including the duplicate
+        }
+
+        [Fact]
+        public void ShouldReturnEmptyListForUnavailableFlowEndpoint()
+        {
+            // Arrange
+            var service = new FlowManager();
+
+            // Act
+            var packets = service.GetAllFlowsByEndpoint(new FlowEndpoint(IPAddress.Parse("3.3.3.3"), 44, PhysicalAddress.None));
+
+            // Assert
+            packets.Should().BeEmpty(); // Expecting an empty list
+        }
+
+        [Fact]
+        public void ShouldRetrieveAllPacketsForGivenFlowEndpoint()
+        {
+            // Arrange
+            var service = new FlowManager();
+
+            var packet1 = CreatePacket(
+                IPAddress.Parse("1.1.1.1"), 22,
+                IPAddress.Parse("8.8.8.8"), 99);
+
+            var packet2 = CreatePacket(
+                IPAddress.Parse("2.2.2.2"), 33,
+                IPAddress.Parse("8.8.8.8"), 99);
+
+            var packet3 = CreatePacket(
+                IPAddress.Parse("8.8.8.8"), 99,
+                IPAddress.Parse("1.1.1.1"), 22);
+
+            // Adding packet with reverse direction to test the flow consistency
+            var packet4 = CreatePacket(
+                IPAddress.Parse("8.8.8.8"), 99,
+                IPAddress.Parse("2.2.2.2"), 33);
+
+            var packet5 = CreatePacket(
+                IPAddress.Parse("1.1.1.1"), 22,
+                IPAddress.Parse("7.7.7.7"), 80);
+
+            var packet6 = CreatePacket(
+                IPAddress.Parse("3.3.3.3"), 4444,
+                IPAddress.Parse("5.5.5.5"), 6666);
+
+            // Act
+            service.AddPacket(packet1);
+            service.AddPacket(packet2);
+            service.AddPacket(packet3);
+            service.AddPacket(packet4);
+            service.AddPacket(packet5);
+            service.AddPacket(packet6);
+
+            // Assert
+            var endpointToQuery = packet4.FlowKey.Endpoint1;
+            var packetsForEndpoint = service.GetAllFlowsByEndpoint(endpointToQuery);
+
+            packetsForEndpoint.Should().HaveCount(4);
+        }
+
+    }
+}
